46cd5dc8226de3cd871d816d9b5fdc741c9ebd53
Nishanth Duraiarasu <dnishanth99@gmail.com>
2022-10-16 21:20:28 -0400

    Created component structure for challenge section

---
+++ public/tuiter/PostList/posts.js
@@ -0,0 +1,19 @@
+export default [
+    {
+        author: 'Elon Musk',
+        handle: 'elonmusk',
+        time: '23h',
+        body: 'React.js is a component based front end library that makes it very easy to build Single Page Applications or SPAs',
+        image: 'react.jpg',
+        image_title: 'Countdown: Inspiration4 Mission to Space | Netflix Official Site',
+        image_body: 'From training to launch to landing, this all-access docuseries rides along with the Inspiration4 crew on the first all-civilian orbital space ...',
+        image_link: 'netflix.com',
+    },
+    {
+        author: 'New York Post',
+        handle: 'nypost',
+        time: '23h',
+        body: 'Grimes seen reading Karl Marx following split with world\'s richest man Elon Musk ',
+        body_link: 'trib.al/nx2Gfaq',
+        image: 'react.jpg',
+    },];
