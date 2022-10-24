6119a2425bc107c533e94e04d0f0d92d88d1c331
Nishanth Duraiarasu <dnishanth99@gmail.com>
2022-10-23 23:51:43 -0400

    Test anchor /# fix

--- src/tuiter/explore/index.js
+++ src/tuiter/explore/index.js
@@ -18,18 +18,17 @@ const ExploreComponent = () => {
             </div>
             <ul className="nav nav-pills mb-2">
                 <li className="nav-item">
-                    {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                    <a className="nav-link active">For You</a>
+                    <a href="/#" className="nav-link active">For You</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link">Trending</a>
+                    <a href="/#" className="nav-link">Trending</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link">News</a>
+                    <a href="/#" className="nav-link">News</a>
                 </li>
             </ul>
             <div className="position-relative mb-2">
-                <img src="/images/spacex.webp" className="w-100"/>
+                <img src="/images/spacex.webp" className="w-100" alt="SpaceX"/>
                 <h1 className="position-absolute wd-nudge-up text-white">
                     SpaceX Starship</h1>
             </div>
